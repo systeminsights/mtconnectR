71474a823f305426c5394745fdbdbbd76df61b63
alexsanjoseph <alex@systeminsights.com>
2016-02-04 11:07:04 -0500

    standardized names in merge.R

--- R/merge.R
+++ R/merge.R
@@ -1,41 +1,37 @@
 
 #' Merges all the data.frames in the list into single data.frame
 #' 
-#' @param dataList is a list of data.frames. Each data.frame should be of type timestamp|value1|value2...
-#' @param outputDF if TRUE, then returns output in the form of data.frame instead of data.table
-mergeTS <- function(dataList, outputDF = T, useListNames = F, missTimes = integer())
-{
-  a = NULL
+#' @param DF_list is a list of data.frames. Each data.frame should be of type timestamp|value1|value2...
+#' @param output_DF if TRUE, then returns output in the form of data.frame instead of data.table
+#' @param use_list_names if TRUE, the names of the list are assigned the columns names
+#' @param additional_ts an POSIXct vector of timestamps which needs to be added into the table.
+#'  The values are repeated from the previous timestamp
+#' 
+mergeTS <- function(DF_list, output_DF = T, use_list_names = F, additional_ts = integer()){
   
-  if (length(dataList) == 0) 
-  {
-    flog.info(paste("You gave me a list with zero elements!"))
+  if (length(dataList) == 0) {
+    warn(paste("You gave me a list with zero elements!"))
     return(NULL)
   }
   
+  all_timestamps = NULL
   for (i in setdiff(1:length(dataList), missTimes))
-    a = append(a, dataList[[i]]$timestamp)
+    all_timestamps = append(all_timestamps, dataList[[i]]$timestamp)
   
-  seed = data.table(timestamp = sort(unique(a), method = "quick"),key = "timestamp")
+  merged_data = data.table(timestamp = sort(unique(a), method = "quick"),key = "timestamp")
   
-  #   i = 1
-  
-  for (i in length(dataList):1)
-  {
+  for (i in length(dataList):1){
     data = dataList[[i]]
     data = data.table(data, key = "timestamp")
-    
-    seed = data[seed, mult = "first", roll = T]
-    
-    setnames(seed ,c("timestamp", paste0("V", 2:ncol(seed)))) 
-    
+    merged_data = data[merged_data, mult = "first", roll = T]
+    setnames(merged_data ,c("timestamp", paste0("V", 2:ncol(merged_data)))) 
   }
+  
   if (!useListNames)  valNames = unlist(sapply(dataList, names)) else
     valNames = unlist(names(dataList))
   
   valNames = valNames[valNames!="timestamp"]
-  setnames(seed, c("timestamp", valNames))
-  data_output = seed
-  if (outputDF == T) data_output = data.frame(data_output)
-  (data_output)
+  setnames(merged_data, c("timestamp", valNames))
+  if (outputDF == T) merged_data = data.frame(merged_data)
+  merged_data
 }
