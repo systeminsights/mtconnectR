e6fad8b11c19fb3ccfa0c859adc7bfc385d32868
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-05 11:58:34 -0500

    common file to store common functions

---
+++ R/common.R
@@ -0,0 +1,18 @@
+
+# Common functions across the package
+
+#' @importFrom data.table rbindlist
+#' @importFrom stringr str_split
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_replace
+#' @importFrom stringr str_detect
+#' @importFrom stringr str_detect
+#' @importFrom dplyr arrange
+#' @importFrom dplyr '%>%' 
+#' @importFrom dplyr mutate 
+#' @importFrom dplyr arrange
+#' @importFrom dplyr arrange
+
+dummy_function <- function(){
+  return(NULL)
+}
