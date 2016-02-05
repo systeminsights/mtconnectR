414bb66ec4a9ee106e6f309ccbe2fed5b516f744
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-05 12:24:06 -0500

    Adding the first tests

--- tests/testthat/test-dataExtraction.R
+++ tests/testthat/test-dataExtraction.R
@@ -1,5 +1,11 @@
 
-library(testthat)
-library(mtconnectR)
+library("testthat")
 
-test_check("vayu")
+context("find_line_type")
+log_data = c("*This is a command",
+             "2015-01-01T07:07:07|This is time series",
+             "2015-01-01T07:07:07|@ASSET@|",
+             "Unknown Type")
+
+expect_equal(vapply(log_data, find_line_type, ""), c("COMMAND", "TS", "ASSET", "UNKNOWN"))
+
