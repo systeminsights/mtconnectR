b7ab668d9cba20263dfcd2a8c2a795f54a750ca3
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-05 11:58:21 -0500

    sandbox

---
+++ sandbox.R
@@ -0,0 +1,7 @@
+
+file_path_adapter_log = "../data/adapter_logs/tft/tft-405-pfh.log"
+file_path_xml = "../data/adapter_logs/tft/Devices.xml.txt"
+device_xml_name = "TFT-405-PFH"
+
+A = create_mtcdevice_from_adapter_data(file_path_adapter_log, file_path_xml, device_xml_name)
+
