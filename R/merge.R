3786a6961ef05095cd31fd124a4c27cd5234456a
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-07 21:51:28 -0500

    Fixed tests and data items

--- R/merge.R
+++ R/merge.R
@@ -6,32 +6,32 @@
 #' @param use_list_names if TRUE, the names of the list are assigned the columns names
 #' @param additional_ts an POSIXct vector of timestamps which needs to be added into the table.
 #'  The values are repeated from the previous timestamp
-#' 
+#' @importFrom data.table setnames
+#' @export
 mergeTS <- function(DF_list, output_DF = T, use_list_names = F, additional_ts = integer()){
   
-  if (length(dataList) == 0) {
+  if (length(DF_list) == 0) {
     warn(paste("You gave me a list with zero elements!"))
     return(NULL)
   }
   
   all_timestamps = NULL
-  for (i in setdiff(1:length(dataList), missTimes))
-    all_timestamps = append(all_timestamps, dataList[[i]]$timestamp)
+  for (i in setdiff(1:length(DF_list), additional_ts))
+    all_timestamps = append(all_timestamps, DF_list[[i]]$timestamp)
   
-  merged_data = data.table::data.table(timestamp = sort(unique(a), method = "quick"),key = "timestamp")
-  
-  for (i in length(dataList):1){
-    data = dataList[[i]]
-    data = data.table::data.table(data, key = "timestamp")
-    merged_data = data[merged_data, mult = "first", roll = T]
-    setnames(merged_data ,c("timestamp", paste0("V", 2:ncol(merged_data)))) 
+  merged_data = data.table::data.table(timestamp = sort(unique(all_timestamps), method = "quick"),key = "timestamp")
+  for (i in length(DF_list):1){
+    single_data = DF_list[[i]]
+    single_data = data.table::data.table(single_data, key = "timestamp")
+    merged_data = single_data[merged_data, mult = "first", roll = T]
+    data.table::setnames(merged_data ,c("timestamp", paste0("V", 2:ncol(merged_data)))) 
   }
   
-  if (!useListNames)  valNames = unlist(sapply(dataList, names)) else
-    valNames = unlist(names(dataList))
+  if (!use_list_names)  valNames = unlist(sapply(DF_list, names)) else
+    valNames = unlist(names(DF_list))
   
   valNames = valNames[valNames!="timestamp"]
-  setnames(merged_data, c("timestamp", valNames))
-  if (outputDF == T) merged_data = data.frame(merged_data)
+  data.table::setnames(merged_data, c("timestamp", valNames))
+  if (output_DF == T) merged_data = data.frame(merged_data)
   merged_data
 }
