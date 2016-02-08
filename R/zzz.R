5581bfaa6ba2416b63f0798edcb35d7c5667e688
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-07 22:47:49 -0500

    Tests also passing

--- R/zzz.R
+++ R/zzz.R
@@ -6,10 +6,3 @@
   data.table::setNumericRounding(0L)
 }
 
-# From hadley:
-# Whenever you use C++ code in your package, 
-# you need to clean up after yourself when your package is unloaded. 
-# Do this by writing a .onUnload() function that unloads the DLL
-.onUnload <- function(libpath) {
-  library.dynam.unload("vayu", libpath)
-}
--- tests/testthat/test-dataExtraction.R
+++ tests/testthat/test-dataExtraction.R
@@ -10,10 +10,14 @@ log_data = c("*This is a command",
 expect_equal(vapply(log_data, find_line_type, "", USE.NAMES = F), c("COMMAND", "TS", "ASSET", "UNKNOWN"))
 
 context("create_device_from_adapter_data")
-file_path_adapter_log = "extdata/tft-405-pfh.log"
-file_path_xml = "extdata/Devices.xml.txt"
-device_xml_name = "TFT-405-PFH"
+file_path_adapter_log = "tests/dataExtraction/test_log_data.log"
+file_path_xml = "tests/dataExtraction/test_devices.xml"
+device_xml_name = "test_device"
 mtc_device = create_mtc_device_from_adapter_data(
   system.file(file_path_adapter_log, package = "mtconnectR"),
   system.file(file_path_xml, package = "mtconnectR"),
   device_xml_name)
+
+expected = readRDS(system.file("tests/dataExtraction/test_device_mtcdevice.rds", package = "mtconnectR"))
+
+expect_equal(mtc_device, expected)
