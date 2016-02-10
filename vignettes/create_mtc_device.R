d5a825fa6f15d14afa74d93c1d989bc2a5bf6346
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-10 13:03:02 -0500

    Updated documentation

--- vignettes/create_mtc_device.R
+++ vignettes/create_mtc_device.R
@@ -3,12 +3,48 @@ require(mtconnectR)
 file_path_adapter_log = "tests/dataExtraction/test_log_data.log"
 file_path_xml = "tests/dataExtraction/test_devices.xml"
 device_name = "test_device"
-
 mtc_device = create_mtc_device_from_adapter_data(
   system.file(file_path_adapter_log, package = "mtconnectR"),
   system.file(file_path_xml, package = "mtconnectR"),
   device_name)
 
+
+## ------------------------------------------------------------------------
+# Get the first data item in the list
+mtc_data_item = getDataItem(mtc_device)
+print(mtc_data_item)
+
+# Get the first data item in the list
+mtc_data_item = getDataItem(mtc_device, "Xabs")
+print(mtc_data_item)
+
+# Get the first data item in the list
+mtc_data_item = getDataItem(mtc_device, 5)
+print(mtc_data_item)
+
+# Get all data items with path matching the string 'POSITION'
+mtc_data_item_list = getDataItem(mtc_device, "POSITION")
+print(mtc_data_item)
+
+## ------------------------------------------------------------------------
+
 print(summary(mtc_device))
+print(summary(mtc_data_item))
+
+
+## ------------------------------------------------------------------------
+
+mtc_device_data = getData(mtc_device)
+print(mtc_device_data)
+
+mtc_data_item_data = getData(mtc_data_item)
+print(mtc_data_item_data)
+
+
+## ------------------------------------------------------------------------
+
+print(merge(mtc_device))
+print(merge(mtc_device, "POSIT"))
+print(merge(mtc_device, 3:4))
 
 
