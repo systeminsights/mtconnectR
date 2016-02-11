2dad1831476ed8605bdfe330c26800ae737bb707
alexsanjoseph <alex@systeminsights.com>
2016-02-11 13:16:58 -0500

    updated_docs

--- vignettes/create_mtc_device.R
+++ vignettes/create_mtc_device.R
@@ -1,7 +1,7 @@
 ## ------------------------------------------------------------------------
 require(mtconnectR)
-file_path_adapter_log = "tests/dataExtraction/test_log_data.log"
-file_path_xml = "tests/dataExtraction/test_devices.xml"
+file_path_adapter_log = "testdata/dataExtraction/test_log_data.log"
+file_path_xml = "testdata/dataExtraction/test_devices.xml"
 device_name = "test_device"
 mtc_device = create_mtc_device_from_adapter_data(
   system.file(file_path_adapter_log, package = "mtconnectR"),
