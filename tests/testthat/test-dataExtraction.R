4e6a6f9cf7e28b9ba415b818935f65e99b7f0687
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-11 22:10:47 -0500

    travis tests

--- tests/testthat/test-dataExtraction.R
+++ tests/testthat/test-dataExtraction.R
@@ -40,9 +40,9 @@ context("read_adapter_log_file")
 condition_names = c("servo_cond", "logic_cond")
 log_data = read_adapter_log_file(system.file(file_path_adapter_log, package = "mtconnectR"), condition_names)
 data("example_log_data")
-print(summary(log_data))
-print(summary(example_log_data))
-expect_true(all.equal(log_data, example_log_data))
+print((log_data))
+print((example_log_data))
+expect_true(all.equal(log_data$timestamp, example_log_data$timestamp))
 
 #===============================================================================
 
