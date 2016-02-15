7ccb49a2aced324921afa01e17197ab1046877e4
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-15 23:24:26 +0000

    Bug fix in mergeTS

--- R/merge.R
+++ R/merge.R
@@ -8,18 +8,23 @@
 #'  The values are repeated from the previous timestamp
 #' @importFrom data.table setnames
 #' @export
-mergeTS <- function(DF_list, output_DF = T, use_list_names = F, additional_ts = integer()){
+mergeTS <- function(DF_list, output_DF = T, use_list_names = F, additional_ts = .POSIXct(integer(0))){
   
   if (length(DF_list) == 0) {
     warning(paste("You gave me a list with zero elements!"))
     return(NULL)
   }
   
-  all_timestamps = NULL
+  all_tz = sapply(DF_list, function(x) attr(x$timestamp, "tzone"))
+  if (length(unique(all_tz)) != 1)
+    stop("Multiple timestamps present in input")
+  
+  all_timestamps = additional_ts
   for (i in setdiff(1:length(DF_list), additional_ts))
     all_timestamps = append(all_timestamps, DF_list[[i]]$timestamp)
   
   merged_data = data.table::data.table(timestamp = sort(unique(all_timestamps), method = "quick"),key = "timestamp")
+  
   for (i in length(DF_list):1){
     single_data = DF_list[[i]]
     single_data = data.table::data.table(single_data, key = "timestamp")
@@ -32,6 +37,7 @@ mergeTS <- function(DF_list, output_DF = T, use_list_names = F, additional_ts = 
   
   valNames = valNames[valNames!="timestamp"]
   data.table::setnames(merged_data, c("timestamp", valNames))
+  attributes(merged_data$timestamp)$tzone = all_tz[1]
   if (output_DF == T) merged_data = data.frame(merged_data)
   merged_data
 }
