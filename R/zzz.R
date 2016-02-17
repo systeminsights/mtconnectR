64f6c62b1ff63be702ff8788ea25a1ca5d190bad
alexsanjoseph <alexsanjoseph@gmail.com>
2016-02-17 20:59:40 +0000

    Some tests for the classes

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
-.onLoad <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) { # nocov start
   options(stringsAsFactors = FALSE)
   options(digits.secs = 6)
   options(scipen = 4)
   options(digits = 10)
   data.table::setNumericRounding(0L)
-}
+} # nocov end
 
-Sys.setenv("TZ" = "UTC")
+Sys.setenv("TZ" = "UTC") # nocov
