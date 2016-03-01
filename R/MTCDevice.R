281e36584db5b388496b71fd97d1e6a8d28333d7
alexsanjoseph <alexsanjoseph@gmail.com>
2016-03-01 23:08:22 +0000

    Time series to MTC device conversion

--- R/MTCDevice.R
+++ R/MTCDevice.R
@@ -9,3 +9,28 @@ setMethod("initialize", "MTCDevice", function(.Object, data_item_list = list(), 
     .Object@device_uuid = device_uuid
   return(.Object)
 })
+
+
+#' Create an MTC device object from a merged time series data frame
+#' 
+#' @param merged_device An existing object of MTCDevice Class
+#' @param device_uuid UUID to be given to the device
+#' @examples 
+#' data("example_mtc_device")
+#' merged_device = merge(example_mtc_device)
+#' ts_to_mtc_device(merged_device)
+#' 
+#' @export
+ts_to_mtc_device <- function(merged_device, device_uuid = "unmerged_device"){
+  data_item_names = setdiff(names(merged_device), "timestamp")
+  data_item_list = lapply(data_item_names, function(x){
+    temp_df = data.frame(timestamp = merged_device$timestamp, value = merged_device[[x]]) %>% 
+      cleanReduntantRows()
+    new("MTCDataItem", temp_df,
+        data_type = ifelse(test = is.numeric(temp_df$value), yes = 'Sample', no = 'Event'),
+        path = x, dataSource = "Unmerged", xmlID = "") 
+  })
+  names(data_item_list) = data_item_names
+  new('MTCDevice', rawdata = list(), data_item_list = data_item_list, device_uuid = device_uuid)
+  
+}
