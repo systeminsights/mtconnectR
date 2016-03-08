d8f63773a1be7b62b7ce71ac23735dd89cd56688
alexsanjoseph <alexsanjoseph@gmail.com>
2016-03-08 16:15:28 -0500

    Better error checking

--- scripts/get_data.R
+++ scripts/get_data.R
@@ -1,17 +1,26 @@
 
 if(!require("mtconnectR")) devtools::install_github("systeminsights/mtconnectR")
+library(mtconnectR)
+library(dplyr)
 
 args = commandArgs(trailingOnly = T)
-file_path_dmtcd = args[1]
-file_path_xml = args[1]
+file_path_dmtcd = args[1] %>% normalizePath()
+file_path_xml = args[2] %>% normalizePath()
 device_name = args[3]
-file_path_output = args[4]
+file_path_output = args[4] 
 type = args[5]
-if(type == "") type = "long"
 
-mtc_device = create_mtc_device_from_dmtcd(file_path_dmtcd, file_path_xml, device_names)
+if(is.na(type)) type = "long"
+if(is.na(file_path_output)) file_path_output = "output.csv" 
+
+message("Creating data of ", type, " format for device: ", device_name, " from DMTCD file: ", file_path_dmtcd,
+        " using XML file: ", file_path_xml, " for contextualization into output file: ", file_path_output)
+
+mtc_device = create_mtc_device_from_dmtcd(file_path_dmtcd, file_path_xml, device_name)
 
 if(type == "long") output = getData(mtc_device)
 if(type == "wide") output = merge(mtc_device)
 
 write.csv(output, file_path_output, row.names = F)
+
+
