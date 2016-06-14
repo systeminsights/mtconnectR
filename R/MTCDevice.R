2c97ae1cb90b03ecac40241d5ad104d7e8c954ef
alexsanjoseph <alexsanjoseph@gmail.com>
2016-06-14 15:28:57 +0530

    Changed documentaion. Major trimming work

--- R/MTCDevice.R
+++ R/MTCDevice.R
@@ -45,3 +45,52 @@ create_mtc_device_from_ts <- function(merged_device, device_uuid = "unmerged_dev
   new('MTCDevice', rawdata = list(), data_item_list = data_item_list, device_uuid = device_uuid)
   
 }
+
+
+#' Calculate feed rate from the path position data items
+#'
+#' Returns a data.frame which contains the calculated feed rates and the corresponding time stamps
+#' @param mtc_device is the MTCDevice object
+#' @param pattern is the pattern of the path postion data items in the device object
+#' @examples
+#' data("example_mtc_device_3")
+#' calculated_feed_from_postion = calculated_feed_from_position(example_mtc_device_3)
+#' @export
+calculated_feed_from_position <- function(mtc_device, pattern = "PATH_POSITION"){
+  value = NULL
+  # TODO: Quickfix function. Needs to be made generic
+  # Calculate moving average
+  ma <- function(x,n=5){stats::filter(x,rep(1/n,n), sides=2)}
+  
+  posParams = extract_param_from_xpath(names(mtc_device@data_item_list[grep(pattern, names(mtc_device@data_item_list))]))
+  message("Using Datatems ", paste0(posParams, collapse = ","), " for calculating PFR")
+  positions_merged = convert_ts_to_interval(merge(mtc_device, pattern))
+  positions_merged_diff = apply(positions_merged[,4:ncol(positions_merged)], 2, diff)
+  positions_merged_diff_sum = c(0, round(sqrt(apply(positions_merged_diff, 1, function(x)  sum(x*x) )),4))
+  calculated_feed = data.frame(timestamp = positions_merged$start, value = positions_merged_diff_sum * 60 / positions_merged$duration) %>%
+    mutate(value = ma(value, 3)) %>% # Not correct, but I don't care now
+    stats::na.omit() %>% clean_reduntant_rows()
+}
+
+#' Filter MTCDevice object based on time range
+#'
+#' Helper function to quickly filter based on time range
+#' @param mtc_device is the MTCDevice object
+#' @param start_time is the Start time
+#' @param end_time is the End time
+#' @examples 
+#' data("example_mtc_device_3")
+#' start_time = as.POSIXct("2016-03-22 12:45:00.000")
+#' end_time = as.POSIXct("2016-03-22 12:45:10.000")
+#' filtered_data = filter_timestamps_mtc_device(example_mtc_device_3,start_time,end_time)
+#' @export
+filter_timestamps_mtc_device <- function(mtc_device, start_time, end_time){
+  timestamp = NULL
+  mtc_device@data_item_list = lapply(mtc_device@data_item_list, function(x){
+    x@data = x@data %>%  filter(timestamp > start_time & timestamp < end_time)
+    x
+  })
+  # mtc_device@rawdata = mtc_device@rawdata %>% as.data.frame %>%  
+  #   filter(timestamp > start_time & timestamp < end_time) %>% as.list
+  mtc_device
+}
--- scripts/nist_test_bed.R
+++ scripts/nist_test_bed.R
@@ -1,3 +1,5 @@
+# This has a 
+
 library(mtconnectR)
 library(ggplot2)
 library(dplyr)
@@ -19,126 +21,57 @@ pos_data_mtc = merge(mtc_device, "path_pos") %>% filter(timestamp > as.POSIXct("
 names(pos_data_mtc) = extract_param_from_xpath(names(pos_data_mtc), show_warnings = F)
 
 # Reading G Code data
-gcode_file_path_1 = "inst/extdata/raw_gcode.NC"
-# gcode_file_path_2 = "../data/nist_test_bed/planned/the_part/827-9999-904.H OPERATION #2.NC"
-# gcode_file_path_2 = "../data/nist_test_bed/planned/827-9999-904, OP2.NC"
+gcode_file_path = "inst/extdata/raw_gcode.NC"
+gcode_parsed = parse_gcode(gcode_file_path)
+simulated_gcode_data = simulate_data_from_gcode(gcode_parsed, start_time = 0, data_res = 0.1, data_type = "HH") %>%   na.omit()
 
-gcode_parsed_1 = parse_gcode(gcode_file_path_1)
-# gcode_parsed_2 = parse_gcode(gcode_file_path_2)
+mtc_device_sim = create_mtc_device_from_ts(simulated_gcode_data)
+pos_data_sim = merge(mtc_device_sim, "pos")
 
-# Simulating G Code data
-simulated_gcode_data_1 = simulate_data_from_gcode(gcode_parsed_1, start_time = 0, data_res = 0.1, data_type = "HH") %>% 
-  na.omit()
-# simulated_gcode_data_2 = simulate_data_from_gcode(gcode_parsed_2, start_time = 0, data_res = 0.1, data_type = "HH") %>% 
-#   na.omit()
-
-mtc_device_sim_1 = create_mtc_device_from_ts(simulated_gcode_data_1)
-# mtc_device_sim_2 = create_mtc_device_from_ts(simulated_gcode_data_2)
-
-# pos_data_sim_2 = merge(mtc_device_sim_2, "pos")
-pos_data_sim_1 = merge(mtc_device_sim_1, "pos")
 # Subsetting individual operations in MTC data
 ggplot(pos_data_mtc) + geom_path(aes(x = timestamp, y = path_pos_y)) 
 
-# time_start = as.POSIXct("2016-03-22 08:30:00", tz = "UTC") 
-# time_split_1 = as.POSIXct("2016-03-22 10:30:00", tz = "UTC") 
-time_split_2 = as.POSIXct("2016-03-22 12:45:00", tz = "UTC") 
-time_split_3 = as.POSIXct("2016-03-22 13:00:00", tz = "UTC") 
-# time_end = as.POSIXct("2016-03-22 15:30:00", tz = "UTC") 
+time_split_start = as.POSIXct("2016-03-22 12:45:00", tz = "UTC") 
+time_split_end = as.POSIXct("2016-03-22 13:00:00", tz = "UTC") 
 
-# pos_data_mtc_1 = pos_data_mtc %>% filter(timestamp > time_start   & timestamp < time_split_1)
-# pos_data_mtc_2 = pos_data_mtc %>% filter(timestamp > time_split_1 & timestamp < time_split_2)
-pos_data_mtc_3 = pos_data_mtc %>% filter(timestamp > time_split_2 & timestamp < time_split_3)
-# pos_data_mtc_4 = pos_data_mtc %>% filter(timestamp > time_split_3 & timestamp < time_end)
+pos_data_mtc = pos_data_mtc %>% filter(timestamp > time_split_start & timestamp < time_split_end)
 
-# ggplot(pos_data_mtc_1) + geom_path(aes(x = path_pos_x, y = path_pos_y)) 
-# ggplot(pos_data_mtc_2) + geom_path(aes(x = path_pos_x, y = path_pos_y))
-ggplot(pos_data_mtc_3) + geom_path(aes(x = path_pos_x, y = path_pos_y)) 
-# ggplot(pos_data_mtc_4) + geom_path(aes(x = path_pos_x, y = path_pos_y)) 
+ggplot(pos_data_mtc) + geom_path(aes(x = path_pos_x, y = path_pos_y)) 
 
 # Plotting positions data
-ggplot(pos_data_sim_1) + geom_path(aes(x = x_pos, y = y_pos)) +
+ggplot(pos_data_sim) + geom_path(aes(x = x_pos, y = y_pos)) +
   coord_cartesian(xlim = c(-2.5, 6), ylim = c(-4, 1))
-ggsave("../data/nist_test_bed/results/1_op1_sim.png", width = 15, height = 10, dpi = 500)
 
-ggplot(pos_data_mtc_3) + geom_path(aes(x = path_pos_x, y = path_pos_y)) +
+ggplot(pos_data_mtc) + geom_path(aes(x = path_pos_x, y = path_pos_y)) +
   coord_cartesian(xlim = c(-2.5, 6), ylim = c(-4, 1))
-# ggsave("../data/nist_test_bed/results/2_op1_mtc.png", width = 15, height = 10, dpi = 500)
-# 
-# ggplot(pos_data_sim_2) + geom_path(aes(x = x_pos, y = y_pos)) +
-#   coord_cartesian(xlim = c(-5, 1), ylim = c(-3, 1))
-# ggsave("../data/nist_test_bed/results/3_op2_sim.png", width = 15, height = 10, dpi = 500)
-# 
-# ggplot(pos_data_mtc_2) + geom_path(aes(x = path_pos_x, y = path_pos_y)) +
-#   coord_cartesian(xlim = c(-5, 1), ylim = c(-3, 1))
-# ggsave("../data/nist_test_bed/results/4_op2_mtc_1.png", width = 15, height = 10, dpi = 500)
-# 
-# ggplot(pos_data_mtc_4) + geom_path(aes(x = path_pos_x, y = path_pos_y)) +
-#   coord_cartesian(xlim = c(-5, 1), ylim = c(-3, 1))
-# ggsave("../data/nist_test_bed/results/5_op2_mtc_2.png", width = 15, height = 10, dpi = 500)
 
 # Mapping
+time_start_op = as.POSIXct("2016-03-22 12:45:00", tz = "UTC")
+time_end_op = as.POSIXct("2016-03-22 13:00:00", tz = "UTC")
 
-time_start_op1 = as.POSIXct("2016-03-22 12:45:00", tz = "UTC")
-time_end_op1 = as.POSIXct("2016-03-22 13:00:00", tz = "UTC")
-# time_start_op2_1 = as.POSIXct("2016-03-22 11:20:00", tz = "UTC")
-# time_end_op2_1 = as.POSIXct("2016-03-22 11:40:00", tz = "UTC")
-# time_start_op2_2 = as.POSIXct("2016-03-22 14:00:00", tz = "UTC")
-# time_end_op2_2 = as.POSIXct("2016-03-22 14:20:00", tz = "UTC")
+mtc_device_op = mtc_device %>% filter_timestamps_mtc_device(time_start_op, time_end_op)
+mtc_sim_mapped_op = map_gcode_mtc(mtc_device_sim, mtc_device_op, elasticity = 200)
 
+# Plotting two way
+plot_twoway(mtc_sim_mapped_op, mtc_device_sim, mtc_device_op, 20, 100)
 
-mtc_device_op1_1 = mtc_device %>% filter_timestamps_mtc_device(time_start_op1, time_end_op1)
-# mtc_device_op2_1 = mtc_device %>% filter_timestamps_mtc_device(time_start_op2_1, time_end_op2_1)
-# mtc_device_op2_2 = mtc_device %>% filter_timestamps_mtc_device(time_start_op2_2, time_end_op2_2)
+# Comparing positions data Simulated vs actual 
+feed_compare_op = merge(mtc_sim_mapped_op, "pfr") %>% na.omit()
+names(feed_compare_op) = extract_param_from_xpath(names(feed_compare_op), show_warnings = F)
 
-mtc_sim_mapped_op1 = map_gcode_mtc(mtc_device_sim_1, mtc_device_op1_1, elasticity = 200)
-# mtc_sim_mapped_op2_1 = map_gcode_mtc(mtc_device_sim_2, mtc_device_op2_1, elasticity = 200)
-# mtc_sim_mapped_op2_2 = map_gcode_mtc(mtc_device_sim_2, mtc_device_op2_2, elasticity = 200)
+ggplot(feed_compare_op, aes(x = timestamp)) +
+  geom_step(aes(y = pfr_calculated, colour = 'Calculated'), size = 1) +
+  geom_step(aes(y = pfr, colour = "Simulated"), size = 1) +
+  coord_cartesian(ylim = c(-3, 300))
 
-# Plotting positions data
-plot_twoway(mtc_sim_mapped_op1, mtc_device_sim_1, mtc_device_op1_1, 20, 100)
-# ggsave("../data/nist_test_bed/results/xpos_mapping_mtc_sim_op1.png", width = 15, height = 10, dpi = 500)
-# plot_twoway(mtc_sim_mapped_op2_1, mtc_device_sim_2, mtc_device_op2_1, 20, 100)
-# ggsave("../data/nist_test_bed/results/xpos_mapping_mtc_sim_op2_1.png", width = 15, height = 10, dpi = 500)
-# plot_twoway(mtc_sim_mapped_op2_2, mtc_device_sim_2, mtc_device_op2_2, 20, 100)
-# ggsave("../data/nist_test_bed/results/xpos_mapping_mtc_sim_op2_2.png", width = 15, height = 10, dpi = 500)
+pos_feed_ratio_compare_op = merge(mtc_sim_mapped_op, "pfr|_pos_") %>% na.omit()
+names(pos_feed_ratio_compare_op) = extract_param_from_xpath(names(pos_feed_ratio_compare_op), show_warnings = F)
 
+pos_feed_ratio_compare_op = pos_feed_ratio_compare_op %>%
+  mutate(feed_ratio = abs(round(((pfr_calculated)/(pfr + 1e-6)), 2))) %>%
+  mutate(feed_ratio = replace(feed_ratio, feed_ratio > 1.1, NA_real_))
 
-# Simulated vs actual 
-# feed_compare_op2_2 = merge(mtc_sim_mapped_op2_2, "pfr") %>% na.omit()
-# names(feed_compare_op2_2) = extract_param_from_xpath(names(feed_compare_op2_2), show_warnings = F)
-# 
-# ggplot(feed_compare_op2_2, aes(x = timestamp)) + 
-#   geom_step(aes(y = pfr_calculated, colour = 'Calculated'), size = 1) + 
-#   geom_step(aes(y = pfr, colour = "Simulated"), size = 1) + 
-#   coord_cartesian(ylim = c(-3, 300))
-# ggsave("../data/nist_test_bed/results/feed_compare_op2_2.png", width = 15, height = 10, dpi = 500)
-# 
-# pos_feed_ratio_compare_op2_2 = merge(mtc_sim_mapped_op2_2, "pfr|_pos_") %>% na.omit()
-# names(pos_feed_ratio_compare_op2_2) = extract_param_from_xpath(names(pos_feed_ratio_compare_op2_2), show_warnings = F)
-# 
-# pos_feed_ratio_compare_op2_2 = pos_feed_ratio_compare_op2_2 %>%
-#   mutate(feed_ratio = abs(round(((pfr_calculated)/(pfr + 1e-6)), 2))) %>% 
-#   mutate(feed_ratio = replace(feed_ratio, feed_ratio > 1.1, NA_real_))
-# 
-# ggplot(pos_feed_ratio_compare_op2_2) +
-#   geom_path(aes(x = path_pos_x, y = path_pos_y, col = feed_ratio), size = 1) +
-#   scale_colour_gradient(low="#91bfdb", high = "#fc8d59", na.value = "#ffffbf") + 
-#   coord_cartesian(xlim = c(-5, 1), ylim = c(-3, 0.5))
-# ggsave("../data/nist_test_bed/results/position_feed_ratio_op2_2.png", width = 15, height = 10, dpi = 500)
-
-# Save Data
-# Feed calculated,
-mtc_device_op1_1_merged = merge(mtc_device_op1_1)
-# mtc_device_op2_1_merged = merge(mtc_device_op2_1)
-mtc_sim_mapped_op1_merged = merge(mtc_sim_mapped_op1)
-# mtc_sim_mapped_op2_1_merged = merge(mtc_sim_mapped_op2_1)
-
-# write.csv(mtc_device_op1_1_merged , "../data/nist_test_bed/results/mtc_device_op1_1_merged.csv", row.names = F)
-# write.csv(mtc_device_op1_1_merged, "../data/nist_test_bed/results/mtc_device_op1_1_merged.csv", row.names = F)
-# write.csv(gcode_parsed_1, "../data/nist_test_bed/results/gcode_parsed_1.csv", row.names = F)
-# write.csv(gcode_parsed_2, "../data/nist_test_bed/results/gcode_parsed_2.csv", row.names = F)
-# write.csv(simulated_gcode_data_1, "../data/nist_test_bed/results/simulated_gcode_data_1.csv", row.names = F)
-# write.csv(simulated_gcode_data_2, "../data/nist_test_bed/results/simulated_gcode_data_2.csv", row.names = F)
-# write.csv(mtc_sim_mapped_op1_merged, "../data/nist_test_bed/results/mtc_sim_mapped_op1_merged.csv", row.names = F)
-# write.csv(mtc_sim_mapped_op2_1_merged, "../data/nist_test_bed/results/mtc_sim_mapped_op2_1_merged.csv", row.names = F)
+ggplot(pos_feed_ratio_compare_op) +
+  geom_path(aes(x = path_pos_x, y = path_pos_y, col = feed_ratio), size = 1) +
+  scale_colour_gradient(low="#91bfdb", high = "#fc8d59", na.value = "#ffffbf") +
+  coord_cartesian(xlim = c(-2, 5), ylim = c(-4, 1.5))
