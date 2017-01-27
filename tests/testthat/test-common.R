a569a1746db26f4e7463e8993587d2c2d306f597
alexsanjoseph <alexsanjoseph@gmail.com>
2017-01-27 07:42:05 +0000

    Changed default rounding to 6

--- tests/testthat/test-common.R
+++ tests/testthat/test-common.R
@@ -8,7 +8,7 @@ context("convert_ts_to_interval")
 ts_data = data.frame(ts = as.POSIXct(c(0.5, 1, 1.008, 1.011) + 1445579573,  tz = 'CST6CDT', origin = "1970-01-01"),
                      x = c("a", "b", "c", "d"), y = c("e", "e", "e", "f"))
 expected_interval = data.frame(start = ts_data$ts, end = c(ts_data$ts[2:4], ts_data$ts[1] + 10),
-                               duration = c(0.50, 0.01, 0.00, 9.49),
+                               duration = c(0.500, 0.008, 0.003, 9.489),
                                x = c("a", "b", "c", "d"), y = c("e", "e", "e", "f"))
 interval_data = convert_ts_to_interval(ts_data, time_colname = "ts", endtime_lastrow = ts_data$ts[1] + 10)
 expect_equal(expected_interval, interval_data)
