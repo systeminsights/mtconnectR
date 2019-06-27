1a192c7301a52e1985d02f38d1662c4d67d0b943
devarajphukan <devaraj.phukan@gmail.com>
2019-06-27 05:33:35 +0000

    fixed example

--- R/common.R
+++ R/common.R
@@ -241,12 +241,13 @@ convert_ts_to_interval <- function(df, endtime_lastrow = as.POSIXct(NA), arrange
 #' @seealso \code{\link{convert_ts_to_interval}}
 #' @export
 #' @examples
-# test_interval =
-#   data.frame(start = as.POSIXct(c(0.5, 1, 1.008, 1.011),  tz = 'CST6CDT', origin = "1970-01-01"),
-#              end   = as.POSIXct(c(1, 1.008, 1.011, 2),  tz = 'CST6CDT', origin = "1970-01-01"),
-#              duration = c(0.50, 0.01, 0.00, 0.99),
-#              x     = c("a", "b", "c", "d"),             y     = c("e", "e", "e", "f"))
-# convert_interval_to_ts(test_interval)
+#' test_interval =
+#'   data.frame(start = as.POSIXct(c(0.5, 1, 1.008, 1.011),  tz = 'CST6CDT', origin = "1970-01-01"),
+#'              end   = as.POSIXct(c(1, 1.008, 1.011, 2),  tz = 'CST6CDT', origin = "1970-01-01"),
+#'              duration = c(0.50, 0.01, 0.00, 0.99),
+#'              x     = c("a", "b", "c", "d"),             
+#'              y     = c("e", "e", "e", "f"))
+#' convert_interval_to_ts(test_interval)
 convert_interval_to_ts <- function(df, time_colname = 'start', end_colname = 'end', remove_last = F)
 {
   df = df %>% arrange(get(time_colname))
